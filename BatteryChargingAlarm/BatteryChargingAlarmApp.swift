760d336ba7f02e1b14d45fa40f85938c2c3e74d3
Jin Park <jinpark1504@gmail.com>
2024-07-21 15:31:03 +0900

    Initial Commit

---
+++ BatteryChargingAlarm/BatteryChargingAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryChargingAlarmApp.swift
+//  BatteryChargingAlarm
+//
+//  Created by Jungjin Park on 2024-07-21.
+//
+
+import SwiftUI
+
+@main
+struct BatteryChargingAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryChargingAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BatteryChargingAlarm
+//
+//  Created by Jungjin Park on 2024-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
